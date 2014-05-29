30813f577ac4879a8f053337b77df427d02094a4
Makoto <ChrisAnonymously@gmail.com>
2014-05-28 15:30:18 -1000

    First Commit

---
+++ scripts/expenditures.js
@@ -0,0 +1,318 @@
+
+/*It is recommended that you make the Company: "Name" less than 32 characters. */
+var p_data = [{
+    Company: "Accoutning",
+    TotalRevenue: 29011000,
+    TotalProfit: 12214,
+    RevPerSecond: 10.91993277524,
+    ProfPerSecond: 387.303399289701,
+    RevenueRadius: 190.34,
+    ProfitRadius: 86.01,
+	CompanyLogoPath: "http://onecupofjava.com/hibudget/images/logos/accounting.png",
+    Source: "http://hdoa.hawaii.gov",
+    Color: "#d74631"
+}, {
+    Company: "Department of Agriculture",
+    TotalRevenue: 129813000,
+    TotalProfit: 1500,
+    RevPerSecond: 4.11634322679,
+    ProfPerSecond: 47.5646879756469,
+    RevenueRadius: 69.05,
+    ProfitRadius: 30.14,
+	CompanyLogoPath: "http://onecupofjava.com/hibudget/images/logos/agriculture.png",
+    Source: "https://www.google.co.uk/finance?q=NASDAQ%3AFB&fstype=ii&ei=3nYEU9i7KOrEwAOtBA",
+    Color: "#7ebc01"
+}, {
+    Company: "Hawaii Attorney General",
+    TotalRevenue: 56462000,
+    TotalProfit: -645.32,
+    RevPerSecond: 1.79039827499,
+    ProfPerSecond: -20.462962962963,
+    RevenueRadius: 20.07,
+    ProfitRadius: -19.77,
+	CompanyLogoPath: "http://onecupofjava.com/hibudget/images/logos/HawaiiAG.png",
+    Source: "http://ag.hawaii.gov",
+    Color: "#12678c"
+}, {
+    Company: "Apple",
+    TotalRevenue: 176219000,
+    TotalProfit: 37037,
+    RevPerSecond: 0.91993277524,
+    ProfPerSecond: 1174.43556570269,
+    RevenueRadius: 321.72,
+    ProfitRadius: 149.77,
+	CompanyLogoPath: "http://synclis.com/media/symbol-s.png",
+    Source: "http://dbedt.hawaii.gov",
+    Color: "#dbdbdb"
+}, {
+    Company: "Microsoft",
+    TotalRevenue: 77849,
+    TotalProfit: 21863,
+    RevPerSecond: 2468.57559614409,
+    ProfPerSecond: 693.271182141045,
+    RevenueRadius: 217.13,
+    ProfitRadius: 115.07,
+	CompanyLogoPath: "http://synclis.com/media/symbol-s.png",
+    Source: "https://www.google.co.uk/finance?q=NASDAQ%3AMSFT&fstype=ii&ei=g3YEU6jjIOrEwAOtBA",
+    Color: "#3c5597"
+}, {
+    Company: "LinkedIn",
+    TotalRevenue: 1528.55,
+    TotalProfit: 26.77,
+    RevPerSecond: 48.4700025367834,
+    ProfPerSecond: .848871131405378,
+    RevenueRadius: 30.43,
+    ProfitRadius: 4.03,
+	CompanyLogoPath: "http://synclis.com/media/symbol-s.png",
+    Source: "https://www.google.co.uk/finance?q=NYSE%3ALNKD&fstype=ii&ei=QHcEU5juLIaFwAPi-QE",
+    Color: "#007bbd"
+}, {
+    Company: "Amazon",
+    TotalRevenue: 74452,
+    TotalProfit: 745,
+    RevPerSecond: 2360.85743277524,
+    ProfPerSecond: 23.6237950279046,
+    RevenueRadius: 212.34,
+    ProfitRadius: 21.24,
+	CompanyLogoPath: "http://synclis.com/media/symbol-s.png",
+    Source: "https://www.google.co.uk/finance?q=NASDAQ%3AAMZN&fstype=ii&ei=oHYEU_iNGOrEwAOtBA",
+    Color: "#ff9902"
+}, {
+    Company: "Ebay",
+    TotalRevenue: 16047,
+    TotalProfit: 2856,
+    RevPerSecond: 508.84703196347,
+    ProfPerSecond: 90.5631659056317,
+    RevenueRadius: 98.58,
+    ProfitRadius: 41.59,
+	CompanyLogoPath: "http://synclis.com/media/symbol-s.png",
+    Source: "https://www.google.co.uk/finance?q=NASDAQ%3AEBAY&fstype=ii&ei=tXYEU7CKIerEwAOtBA",
+    Color: "#f5af02"
+}, {
+    Company: "Yahoo!",
+    TotalRevenue: 4680.38,
+    TotalProfit: 479.89,
+    RevPerSecond: 148.413876204972,
+    ProfPerSecond: 15.2172120750888,
+    RevenueRadius: 53.24,
+    ProfitRadius: 17.05,
+	CompanyLogoPath: "http://synclis.com/media/symbol-s.png",
+    Source: "https://www.google.co.uk/finance?q=NASDAQ%3AYHOO&fstype=ii&ei=xnYEU9iwAoipwAPNOA",
+    Color: "#6c319d"
+}, {
+    Company: "HP",
+    TotalRevenue: 112298,
+    TotalProfit: 5113,
+    RevPerSecond: 3560.9462201928,
+    ProfPerSecond: 162.132166412988,
+    RevenueRadius: 260.79,
+    ProfitRadius: 55.65,
+	CompanyLogoPath: "http://synclis.com/media/symbol-s.png",
+    Source: "https://www.google.co.uk/finance?q=NYSE%3AHPQ&fstype=ii&ei=KnkEU_jRK8vFwAO2zgE",
+    Color: "#0197d6"
+}, {
+    Company: "Sony",
+    TotalRevenue: 66559.9287,
+    TotalProfit: 1019.5705,
+    RevPerSecond: 2110.6014935312,
+    ProfPerSecond: 32.3303684677828,
+    RevenueRadius: 200.77,
+    ProfitRadius: 24.85,
+	CompanyLogoPath: "http://synclis.com/media/symbol-s.png",
+    Source: "https://www.google.co.uk/finance?q=NYSE%3ASNE&fstype=ii&ei=VHcEU9jfHIH1wAOwcQ",
+    Color: "#666666"
+}, {
+    Company: "Samsung",
+    TotalRevenue: 214968.6,
+    TotalProfit: 28576,
+    RevPerSecond: 6816.6095890411,
+    ProfPerSecond: 906.139015728057,
+    RevenueRadius: 360.82,
+    ProfitRadius: 131.55,
+	CompanyLogoPath: "http://synclis.com/media/symbol-s.png",
+    Source: "http://www.samsung.com/za/news/globalnews/2014/samsung-electronics-announces-fourth-quarter-and-fy-2013-results",
+    Color: "#4d5d9b"
+}];
+$(function () {
+    function a() {
+        $(".start").on("click", function (a) {
+            a.preventDefault(), $(this).parent(".splash").fadeOut(500), $(".toggle-cta").fadeOut(300), $(".companies").fadeIn(200), g(), c(), b(), e(), j.start(), f(), l.start()
+        })
+    }
+
+    function b() {
+        var a = 3900,
+            b = $(".circle");
+        $(".action").on("click", function (c) {
+            c.preventDefault(), $(this).hasClass("pause") ? (j.time() < a && b.addClass("paused"), j.stop(), l.stop(), $(this).attr("title", "Play")) : (b.removeClass("paused"), j.start(), l.start(), $(this).attr("title", "Pause")), $(this).toggleClass("pause").toggleClass("play")
+        })
+    }
+
+    function c() {
+        var a = 300,
+            b = 0,
+            c = 1,
+            d = $(".profitCircle, .profit.number"),
+            e = $(".revenueCircle, .revenue.number"),
+            f = $(".circle, .number");
+        $(".toggle").on("click", function (g) {
+            if (g.preventDefault(), !$(this).hasClass("active")) {
+                $(this).addClass("active").parent().siblings().find("a").removeClass("active");
+                var h = $(this).text().toLowerCase();
+                "profit" == h ? (d.removeClass("disabled").fadeTo(a, c), e.addClass("disabled").fadeTo(a, b)) : "revenue" == h ? (e.removeClass("disabled").fadeTo(a, c), d.addClass("disabled").fadeTo(a, b)) : f.removeClass("disabled").fadeTo(a, c)
+            }
+        })
+    }
+
+    function d(a, b) {
+        this.interval = b;
+        var c, d = 0,
+            e = 0,
+            f = function () {
+                return (new Date).getTime()
+            };
+        this.start = function () {
+            d = d ? d : f(), c = setInterval(a, b)
+        }, this.stop = function () {
+            e = d ? e + f() - d : e, d = 0, clearInterval(c)
+        }, this.reset = function () {
+            e = d = 0
+        }, this.time = function () {
+            return e + (d ? f() - d : 0)
+        }
+    }
+
+    function e() {
+        function a(a) {
+            return a > 9 ? a : ("0" + a).slice(-2)
+        }
+
+        function b(a, b) {
+            var c = [];
+            $(a).each(function (a, b) {
+                c.push(k({
+                    number: b
+                }))
+            }), $(b).html(c.join(""))
+        }
+        var c = j.time(),
+            d = c / 1e3,
+            e = Math.round(d),
+            f = (Math.round(d - e), a(Math.floor(e / 60))),
+            g = a(e % 60),
+            h = ("" + f).split(""),
+            i = ("" + g).split("");
+        b(h, ".counter-minutes"), b(i, ".counter-seconds")
+    }
+
+    function f() {
+        function a(a, b) {
+            $(".inc-" + a).each(function (c) {
+                $(this).text(function () {
+                    var d = Math.round(p_data[c][a + "PerSecond"] * b);
+                    return d >= 0 ? "$" + m(d) : "-$" + m(Math.abs(d))
+                })
+            })
+        }
+        var b = l.time(),
+            c = b / 1e3,
+            d = 1e3 / l.interval,
+            e = Math.round(c * d) / d;
+        a("Prof", e), a("Rev", e)
+    }
+
+    function g() {
+        var a = 1.3,
+            b = 20,
+            c = 2400,
+            d = _.template($("#companyTemplate").html()),
+            e = [];
+        $.each(p_data, function (f, g) {
+			var hexRes = hexToRgb(g.Color);
+			if(!g.Color) g.Color = "#ffffff";
+            e.push(d({
+                companyClass: "BeretaniaCo",
+                company: g.Company,
+				color: g.Color,
+				l: g.CompanyLogoPath,
+				rColor: "background: " + g.Color + "; background: rgba(" + hexRes.r + "," + hexRes.g + "," + hexRes.b + ",0.5)",
+                negP: g.ProfPerSecond < 0 ? "negative" : "",
+                negR: g.RevPerSecond < 0 ? "negative" : "",
+                smallP: g.ProfPerSecond < b ? "small" : "",
+                smallR: g.RevPerSecond < c ? "small" : "",
+                pd: g.ProfitRadius * a,
+                rd: g.RevenueRadius * a
+            }))
+        }), $(".companies").html(e.join(""))
+    }
+	function hexToRgb(hex) {
+		if(!hex) hex = "fff";
+		// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+		var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+		hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+			return r + r + g + g + b + b;
+		});
+
+		var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		return result ? {
+			r: parseInt(result[1], 16),
+			g: parseInt(result[2], 16),
+			b: parseInt(result[3], 16)
+		} : null;
+	}
+	
+
+    function h() {
+        $(".scroll").on("click", function (a) {
+            a.preventDefault();
+            var b = $(this).attr("href");
+            $("html, body").stop().animate({
+                scrollTop: $(b).offset().top - 80
+            }, 900)
+        })
+    }
+
+
+    var j = new d(e, 1e3),
+        k = _.template($("#numberTemplate").html()),
+        l = new d(f, 100),
+        m = function (a) {
+            return a.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        },
+        n = function (a) {
+            var b = $("#sources"),
+                c = _.template($("#sourceTemplate").html()),
+                d = [];
+            $.each(p_data, function (a, b) {
+                d.push(c({
+                    company: b.Company,
+                    url: b.Source
+                }))
+            }), b.find("ul").html(d.join("")), $(a).on("click", function (a) {
+                a.preventDefault(), b.hasClass("visible") ? b.animate({
+                    bottom: "-200px"
+                }, "slow").fadeOut({
+                    queue: !1
+                }).removeClass("visible") : b.animate({
+                    bottom: "0px"
+                }, "slow").fadeIn({
+                    queue: !1
+                }).addClass("visible")
+            })
+        },
+        o = function (a) {
+            var b = $("#embed");
+            $(a).on("click", function (a) {
+                a.preventDefault(), b.hasClass("visible") ? b.animate({
+                    bottom: "-200px"
+                }, "slow").fadeOut({
+                    queue: !1
+                }).removeClass("visible") : b.animate({
+                    bottom: "0px"
+                }, "slow").fadeIn({
+                    queue: !1
+                }).addClass("visible")
+            })
+        };
+    a(), o(".embedLink,.closeEmbed"), n(".sourceLink,.closeSources"), h()
+});
